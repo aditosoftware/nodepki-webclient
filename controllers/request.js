b6160d59b1c2d97e1936fba5e95aff305feb11c8
Thomas Leister <t.leister@adito.de>
2017-03-02 09:20:13 +0100

    Support direct CSR submits

--- controllers/request.js
+++ controllers/request.js
@@ -9,8 +9,6 @@ module.exports = function(req, res) {
     // Wrapper Promise
     return new Promise(function(resolve, reject) {
         // Check auth state
-
-
         var page = {
             title: 'Request new certificate',
             content: {
@@ -20,116 +18,145 @@ module.exports = function(req, res) {
             baseurl: global.config.server.baseurl
         }
 
-        // Create temporary dir
-        var tempdir = 'tmp/' + uuidV4() + '/';
-        fs.ensureDirSync(tempdir);
+
 
 
         new Promise(function(resolve, reject) {
-            if(req.body.submitted && req.body.submitted === 'submitted' && req.session.auth.authed) {
+            if(req.body.submitted && req.session.auth.authed) {
                 page.content.state = 'submitted'
-
-                // Quick! Create key, CSR and submit to server.
-                // Then receive result, and display!
-
-                // Gather data ...
                 var requestdata;
 
-                try {
-                    requestdata = {
-                        key: {
-                            passphrase: req.body.key_passphrase
-                        },
-                        certificate: {
-                            country: req.body.certificate_country,
-                            state: req.body.certificate_state,
-                            locality: req.body.certificate_locality,
-                            organization: req.body.certificate_organization,
-                            cname: req.body.certificate_cname,
-                            lifetime: parseInt(req.body.certificate_lifetime)
+                // Create temporary dir
+                var tempdir = 'tmp/' + uuidV4() + '/';
+                fs.ensureDirSync(tempdir);
+
+                new Promise(function(resolve, reject) {
+
+
+                    if(req.body.submitted === 'submitted_easyform') {
+                        // Quick! Create key, CSR and submit to server.
+                        // Then receive result, and display!
+
+                        try {
+                            requestdata = {
+                                key: {
+                                    passphrase: req.body.key_passphrase
+                                },
+                                certificate: {
+                                    country: req.body.certificate_country,
+                                    state: req.body.certificate_state,
+                                    locality: req.body.certificate_locality,
+                                    organization: req.body.certificate_organization,
+                                    cname: req.body.certificate_cname,
+                                    lifetime: parseInt(req.body.certificate_lifetime)
+                                }
+                            }
+                        } catch(err) {
+                            reject("Not enough data transmitted! Error: " + err)
                         }
-                    }
-                } catch(err) {
-                    reject("Not enough data transmitted! Error: " + err)
-                }
 
 
-                console.log("Gathered data:")
-                console.log(requestdata)
+                        console.log("Gathered data:")
+                        console.log(requestdata)
 
-                // Create certificate key
-                passparam = (requestdata.key.passphrase === '') ? '' : '-aes256 -passout pass:' + requestdata.key.passphrase;
-                exec('openssl genrsa -out key.pem ' + passparam + ' 2048', {
-                    cwd: tempdir
-                }, function(error, stdout, stderr) {
-                    if(!error) {
-                        // Save the key in variable
-                        page.content.key = fs.readFileSync(tempdir + 'key.pem')
-
-                        // Create csr.
-                        passparam = (requestdata.key.passphrase === '') ? '' : '-passin pass:' + requestdata.key.passphrase;
-                        exec('openssl req -config ../../openssl.cnf -key key.pem -new -sha256 -out cert.csr ' + passparam + ' -subj "/C='+requestdata.certificate.country+'/ST='+requestdata.certificate.state+'/L='+requestdata.certificate.locality+'/O='+requestdata.certificate.organization+'/CN='+requestdata.certificate.cname+'"', {
+                        // Create certificate key
+                        passparam = (requestdata.key.passphrase === '') ? '' : '-aes256 -passout pass:' + requestdata.key.passphrase;
+                        exec('openssl genrsa -out key.pem ' + passparam + ' 2048', {
                             cwd: tempdir
                         }, function(error, stdout, stderr) {
                             if(!error) {
-                                // ready
-                                csr = tempdir + 'cert.csr';
-                                log("CSR created. Ready.");
+                                // Save the key in variable
+                                page.content.key = fs.readFileSync(tempdir + 'key.pem')
 
-                                // Send CSR to NodePKI server
-                                // [ ... ]
-                                var csr = fs.readFileSync(tempdir + 'cert.csr', 'utf8');
+                                // Create csr.
+                                passparam = (requestdata.key.passphrase === '') ? '' : '-passin pass:' + requestdata.key.passphrase;
+                                exec('openssl req -config ../../openssl.cnf -key key.pem -new -sha256 -out cert.csr ' + passparam + ' -subj "/C='+requestdata.certificate.country+'/ST='+requestdata.certificate.state+'/L='+requestdata.certificate.locality+'/O='+requestdata.certificate.organization+'/CN='+requestdata.certificate.cname+'"', {
+                                    cwd: tempdir
+                                }, function(error, stdout, stderr) {
+                                    if(!error) {
+                                        // ready
+                                        var csr = fs.readFileSync(tempdir + 'cert.csr', 'utf8');
+                                        log("CSR created. Ready.");
 
-                                var pushdata = {
-                                    data: {
-                                        csr: csr,
-                                        lifetime: requestdata.certificate.lifetime,
-                                        type: 'server'
-                                    },
-                                    auth: {
-                                        username: req.session.auth.username,
-                                        password: req.session.auth.password
-                                    }
-                                }
-
-                                /*
-                                 * Load certificates via API request
-                                 */
-
-                                apiclient.request(global.apipath + '/certificate/request/', 'POST', pushdata ).then(function(response) {
-                                    if(response.success && response.cert) {
-                                        // Cert received from NodePKI server
-                                        log("Cert created successfully.")
-
-                                        page.content.cert = response.cert
-
-                                        // Request intermediate certificate ... we'll display it as well
-                                        apiclient.request(global.apipath + '/ca/cert/get/', 'POST', { data: { ca: 'intermediate' } } ).then(function(response) {
-                                            if(response.success && response.cert) {
-                                                page.content.intermediatecert = response.cert
-                                                resolve(page)
-                                            } else {
-                                                reject("Error while retrieving intermediate cert.")
-                                            }
-                                        })
-                                        .catch(function(err) {
-                                            reject("Error while making API call: ", err)
-                                        });
+                                        resolve(csr)
                                     } else {
-                                        reject("Failed to get certificate.")
+                                        reject("Could not create .csr.");
                                     }
-                                })
-                                .catch(function(err) {
-                                    reject("Error while making API call: ", err)
                                 });
                             } else {
                                 reject("Could not create .csr.");
                             }
                         });
+                    } else if (req.body.submitted === 'submitted_csr') {
+                        // CSR was directly submitted
+
+                        try {
+                            requestdata = {
+                                certificate: {
+                                    lifetime: parseInt(req.body.certificate_lifetime)
+                                }
+                            }
+                        } catch(err) {
+                            reject("Not enough data transmitted! Error: " + err)
+                        }
+
+                        resolve(req.body.csr)
                     } else {
-                        reject("Could not create .csr.");
+                        reject()
                     }
-                });
+                })
+                .then(function(csr) {
+                    fs.removeSync(tempdir);
+                    
+                    // Send CSR to NodePKI server
+                    // [ ... ]
+                    var pushdata = {
+                        data: {
+                            csr: csr,
+                            lifetime: requestdata.certificate.lifetime,
+                            type: 'server'
+                        },
+                        auth: {
+                            username: req.session.auth.username,
+                            password: req.session.auth.password
+                        }
+                    }
+
+                    /*
+                     * Load certificates via API request
+                     */
+
+                    apiclient.request(global.apipath + '/certificate/request/', 'POST', pushdata ).then(function(response) {
+                        if(response.success && response.cert) {
+                            // Cert received from NodePKI server
+                            log("Cert created successfully.")
+
+                            page.content.cert = response.cert
+
+                            // Request intermediate certificate ... we'll display it as well
+                            apiclient.request(global.apipath + '/ca/cert/get/', 'POST', { data: { ca: 'intermediate' } } ).then(function(response) {
+                                if(response.success && response.cert) {
+                                    page.content.intermediatecert = response.cert
+                                    resolve(page)
+                                } else {
+                                    reject("Error while retrieving intermediate cert.")
+                                }
+                            })
+                            .catch(function(err) {
+                                reject("Error while making API call: ", err)
+                            });
+                        } else {
+                            reject("Failed to get certificate.")
+                        }
+                    })
+                    .catch(function(err) {
+                        reject("Error while making API call: ", err)
+                    });
+                })
+                .catch(function(err) {
+                    fs.removeSync(tempdir);
+                    reject(err)
+                })
             } else {
                 /*
                  * Show form
@@ -142,14 +169,12 @@ module.exports = function(req, res) {
         .then(function(page) {
             page.success = true
             res.render('request', page)
-            fs.removeSync(tempdir);
             resolve(page)
         })
         .catch(function(err) {
             page.success = false
             page.errormessage = err
             res.render('request', page)
-            fs.removeSync(tempdir);
             reject(err)
         });
     });
